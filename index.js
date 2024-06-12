8523aa5565834cb66ccfb05fd72efdeae405e317
SandroArgent <msandroezequiel@gmail.com>
2024-06-12 04:43:10 -0300

    se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    let contenedorPersonajes = document.getElementById('personajes');
+
+    fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((datos) => {
+        datos.results.forEach((elementos) => {
+            const contenedorCreado = document.createElement('div');
+            contenedorCreado.innerHTML = `
+                <h4>${elementos.name}</h4>
+                <img src="${elementos.image}">
+                `;
+            contenedorPersonajes.append(contenedorCreado);
+        });
+    });
+});
